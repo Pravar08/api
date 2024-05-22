11b54863d1122b8b8b1a41e921d0217b8120a32a
Pravar08 <pravarmishra08@gmail.com>
2024-05-22 22:34:07 +0530

    firstcommit

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -3,22 +3,23 @@ import React from "react";
 
 interface Props {
   src: string;
-
 }
 
 const ProjectCard = ({ src }: Props) => {
+  // Determine the height for the last image
   const imageHeight = src === "/hotelHospital2.jpg" ? 900 : 1000;
-  return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-      <Image
-        src={src}
-        alt={src}
-        width={1000}
-        height={imageHeight}
-        className="w-full object-contain"
-      />
 
-     
+  return (
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] h-[400px]">
+      <div className="relative" style={{ width: "100%", paddingTop: "100%" }}>
+        <Image
+          src={src}
+          alt={src}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg"
+        />
+      </div>
     </div>
   );
 };
