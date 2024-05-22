9c5f6adee81d8abdd808d8dfec73c3d8f9d79454
Pravar08 <pravarmishra08@gmail.com>
2024-05-22 22:29:45 +0530

    firstcommit

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -7,13 +7,14 @@ interface Props {
 }
 
 const ProjectCard = ({ src }: Props) => {
+  const imageHeight = src === "/hotelHospital2.jpg" ? 900 : 1000;
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <Image
         src={src}
         alt={src}
         width={1000}
-        height={src==="/hotelHospital2.jpg"?900:1000}
+        height={imageHeight}
         className="w-full object-contain"
       />
 
