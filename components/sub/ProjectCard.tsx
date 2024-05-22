1bb6a70c2e92efa4c3434897290415be5f19ea53
Pravar08 <pravarmishra08@gmail.com>
2024-05-22 22:24:19 +0530

    firstcommit

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,25 +1,23 @@
-import React from "react";
 import Image from "next/image";
+import React from "react";
 
 interface Props {
   src: string;
- 
+
 }
 
 const ProjectCard = ({ src }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] w-[300px] h-[400px]">
-      <div className="w-full h-1/2">
-        <Image
-          src={src}
-          alt={src}
-          layout="fill"
-          objectFit="cover"
-          className="object-cover"
-        />
-      </div>
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+      <Image
+        src={src}
+        alt={src}
+        width={1000}
+        height={1000}
+        className="w-full object-contain"
+      />
 
-  
+     
     </div>
   );
 };
