9ec3492ae6b15fbcb0e730cd279835ba3b386799
Pravar08 <pravarmishra08@gmail.com>
2024-05-22 22:06:46 +0530

    firstcommit

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,34 +1,29 @@
+import Image from "next/image";
 import React from "react";
-import ProjectCard from "../sub/ProjectCard";
 
-const Projects = () => {
+interface Props {
+  src: string;
+  title: string;
+  description: string;
+}
+
+const ProjectCard = ({ src, title, description }: Props) => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        Our Client Partnerships
-      </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/manufacturing.png"
-          title="Manufacturing Inventory Solution"
-          description="A comprehensive solution tailored for manufacturing businesses, optimizing inventory management and streamlining processes to maximize efficiency and productivity."
-        />
-        <ProjectCard
-          src="/education.png"
-          title="Empowering Education Enterprises"
-          description="Dedicated solutions designed to empower educational institutions, providing innovative tools and technologies to enhance learning experiences and administrative workflows."
-        />
-        <ProjectCard
-          src="/hotelHospital2.jpg"
-          title="Empowering Hospital And Hotel Solutions"
-          description="Tailored solutions for hospitals and hotels, revolutionizing operations and guest experiences through advanced technology and seamless integration of services."
-        />
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+      <Image
+        src={src}
+        alt={title}
+        width={1000}
+        height={1000}
+        className="w-full object-contain"
+      />
+
+      <div className="relative p-4">
+        <h1 className="text-2xl font-semibold text-white">{title}</h1>
+        <p className="mt-2 text-gray-300">{description}</p>
       </div>
     </div>
   );
 };
 
-export default Projects;
+export default ProjectCard;
