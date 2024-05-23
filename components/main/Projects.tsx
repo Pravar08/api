eccb994db6ac66b9725f7fe2c62a60f38a3e9231
Pravar08 <pravarmishra08@gmail.com>
2024-05-23 17:12:56 +0530

    ChangesPravar logo change

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -3,29 +3,28 @@ import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
+    <div className="flex flex-col items-center justify-center py-20">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Our Client Partnerships
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/manufacturing.png"
-          // title="Manufacturing Solution"
-          // description="A comprehensive solution tailored for manufacturing businesses, optimizing inventory management and streamlining processes to maximize efficiency and productivity."
-        />
-        <ProjectCard
-          src="/education.png"
-          // title="Empowering Education Enterprises"
-          // description="Dedicated solutions designed to empower educational institutions, providing innovative tools and technologies to enhance learning experiences and administrative workflows."
-        />
-        <ProjectCard
-          src="/muntiple.jpg"
-          // title=" Hospital And Hotel Solutions"
-          // description="Tailored solutions for hospitals and hotels, revolutionizing operations and guest experiences through advanced technology and seamless integration of services."
-        />
+      <div className="relative w-full overflow-hidden">
+        <div className="flex gap-10 absolute top-0 left-0 transform transition-transform ease-in-out duration-500">
+          <ProjectCard
+            src="/manufacturing.png"
+            key="manufacturing"
+            // Add any additional props as needed
+          />
+          <ProjectCard
+            src="/education.png"
+            key="education"
+            // Add any additional props as needed
+          />
+          <ProjectCard
+            src="/muntiple.jpg"
+            key="hospital-hotel"
+            // Add any additional props as needed
+          />
+        </div>
       </div>
     </div>
   );
