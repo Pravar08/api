3e26460bc03123dccb0d5db46eef5626c62bdf5b
Pravar08 <pravarmishra08@gmail.com>
2024-05-23 17:47:05 +0530

    ChangesPravar logo change

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,22 +1,24 @@
-import React from "react";
 import Image from "next/image";
+import React from "react";
 
 interface Props {
   src: string;
 }
 
 const ProjectCard = ({ src }: Props) => {
+  // Determine the height for all images
+  const imageHeight = src==="/hotelHospital2.jpg"?420:380; // Adjust this value as needed
+
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] w-[300px] h-[400px]">
-      <div className="w-full h-full relative">
-        <Image
-          src={src}
-          alt={src}
-          layout="fill"
-          objectFit="cover"
-          className="object-cover"
-        />
-      </div>
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] h-[400px]">
+      <Image
+        src={src}
+        alt={src}
+        width={1000}
+        height={imageHeight}
+        layout="responsive"
+        className="object-cover"
+      />
     </div>
   );
 };
