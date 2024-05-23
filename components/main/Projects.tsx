baa7a8371abb7b290c009131808ccde36873f3a9
Pravar08 <pravarmishra08@gmail.com>
2024-05-23 17:43:42 +0530

    ChangesPravar logo change

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,31 +1,32 @@
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
+import styles from "./Projects.module.css"; // Import CSS file for styling
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
+    <div className="flex flex-col items-center justify-center py-20">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Our Client Partnerships
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/manufacturing.png"
-          // title="Manufacturing Solution"
-          // description="A comprehensive solution tailored for manufacturing businesses, optimizing inventory management and streamlining processes to maximize efficiency and productivity."
-        />
-        <ProjectCard
-          src="/education.png"
-          // title="Empowering Education Enterprises"
-          // description="Dedicated solutions designed to empower educational institutions, providing innovative tools and technologies to enhance learning experiences and administrative workflows."
-        />
-        <ProjectCard
-          src="/muntiple.jpg"
-          // title=" Hospital And Hotel Solutions"
-          // description="Tailored solutions for hospitals and hotels, revolutionizing operations and guest experiences through advanced technology and seamless integration of services."
-        />
+      <div className={styles.projectBanner}>
+        <div className={styles.projectCardWrapper}>
+          <ProjectCard
+            src="/manufacturing.png"
+            // Add any additional props as needed
+          />
+        </div>
+        <div className={styles.projectCardWrapper}>
+          <ProjectCard
+            src="/education.png"
+            // Add any additional props as needed
+          />
+        </div>
+        <div className={styles.projectCardWrapper}>
+          <ProjectCard
+            src="/muntiple.jpg"
+            // Add any additional props as needed
+          />
+        </div>
       </div>
     </div>
   );
