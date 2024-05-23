398b6e02ea3a0292576465fa913a05b8ad67e7c6
Pravar08 <pravarmishra08@gmail.com>
2024-05-23 17:30:42 +0530

    ChangesPravar logo change

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -7,14 +7,14 @@ interface Props {
 
 const ProjectCard = ({ src }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] w-[300px] h-[400px]">
-      <div className="w-full h-full relative">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] w-[300px] h-[400px] flex justify-center items-center">
+      <div className="w-[calc(100% - 2rem)] h-[calc(100% - 2rem)] relative">
         <Image
           src={src}
           alt={src}
           layout="fill"
           objectFit="contain"
-          className="object-cover"
+          className="object-cover rounded-lg border border-[#2A0E61]"
         />
       </div>
     </div>
